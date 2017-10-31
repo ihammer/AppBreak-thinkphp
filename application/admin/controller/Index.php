e7ebb83bc3200372a606441ad3cf3aa9c2faf390
wudean <2876483938@qq.com>
2017-10-31 21:16:03 +0800

    初步提交

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/10/31 0031
+ * Time: 20:50
+ */
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    /**
+     * @return mixed
+     * @name 后台首页
+     * @author wudean
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * @return string
+     * @name 欢迎页面
+     * @author wudean
+     */
+    public function welcome(){
+        return 'App 强大的后台！';
+    }
+
+}
